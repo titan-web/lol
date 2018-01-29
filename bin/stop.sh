516f0f4a08f24f9ae456ff19c6ad4ca8f1b82e6d
titan <tjf94432@alibaba-inc.com>
2018-01-29 13:25:35 +0800

    Init

---
+++ bin/stop.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo /export/servers/openresty/nginx/sbin/nginx  -t -c /export/App/nginx-app/config/nginx.conf
+sudo /export/servers/openresty/nginx/sbin/nginx  -s quit -c /export/App/nginx-app/config/nginx.conf
+
+echo "nginx stop"
+echo -e "===========================================\n\n"
+tail -f /export/servers/openresty/nginx/logs/error.log
